b4bfdbe9c1932143d15dde9585a894606e37515e
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2017-11-02 00:12:06 -0200

    Initial commit

---
+++ app/src/main/java/br/fatec/project/violetenergymonitor/SensorListActivity.java
@@ -0,0 +1,13 @@
+package br.fatec.project.violetenergymonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SensorListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_list);
+    }
+}
